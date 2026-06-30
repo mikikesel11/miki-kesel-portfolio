1c6baa611fe443470285bc0eefc8c116a13c16a8
Miki Kesel <miki.kesel11@gmail.com>
2026-06-29 21:51:15 -0400

    Updated Brevo installation to V2

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -48,26 +48,17 @@
 
     @if (config('services.brevo.client_key'))
         {{-- Brevo JS event tracker --}}
-        <script type="text/javascript">
-            (function () {
-                window.sib = { equeue: [], client_key: "{{ config('services.brevo.client_key') }}" };
-                window.sendinblue = {};
-                for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
-                    (function (k) {
-                        window.sendinblue[k] = function () {
-                            var arg = Array.prototype.slice.call(arguments);
-                            (window.sib[k] || function () {
-                                var t = {}; t[k] = arg;
-                                window.sib.equeue.push(t);
-                            })(arg[0], arg[1], arg[2], arg[3]);
-                        };
-                    })(j[i]);
+        <script src="https://cdn.brevo.com/js/sdk-loader.js" async></script>
+        <script>
+            // Version: 2.0
+            window.Brevo = window.Brevo || [];
+            Brevo.push([
+                "init",
+                {
+                client_key: "{{ config('services.brevo.client_key') }}",
+                // Optional: Add other initialization options, see documentation
                 }
-                var n = document.createElement("script"), i = document.getElementsByTagName("script")[0];
-                n.type = "text/javascript", n.id = "sendinblue-js", n.async = !0,
-                    n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key,
-                    i.parentNode.insertBefore(n, i), window.sendinblue.page();
-            })();
+            ]);
         </script>
     @endif
 
